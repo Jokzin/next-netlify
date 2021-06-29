548f89bb229de8788bc3624fb093dcfcf25ddbe7
Halfkan Julien <jookz1@gmail.com>
2021-06-29 17:02:28 +0200

    add dynamic path

--- pages/pokemon/{pokemon}.js
+++ pages/pokemon/{pokemon}.js
@@ -15,8 +15,22 @@ function Pokemon({ pokemon }) {
     )
 }
 
-export async function getStaticProps() {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/charmander')
+export async function getStaticPaths() {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+    const pokemon = await res.json()
+
+    let paths = pokemon.results.map((p) => {
+        return `/pokemon/${p.name}`
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.pokemon}`)
     const pokemon = await res.json()
 
     return {
