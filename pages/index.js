a7b191bff4211e315117238a159789d0ef932a51
Halfkan Julien <jookz1@gmail.com>
2021-06-29 16:19:40 +0200

    test react hook useRouter

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 function Home() {
+    const router = useRouter()
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        router.push('/pokemon')
+    }
+
     return (
         <>
             <Head>
@@ -8,6 +16,7 @@ function Home() {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <div>Welcome</div>
+            <button onClick={handleClick}>Click me !</button>
         </>
     )
 }
