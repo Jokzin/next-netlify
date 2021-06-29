b27b1cc3702889f68939c2d1c2602d1b7009fbb2
Halfkan Julien <jookz1@gmail.com>
2021-06-29 23:35:25 +0200

    Measure perf
    do thing when some metric showed up

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,26 @@ import '../styles.css'
 export default function Application({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
+
+export function reportWebVitals(metric) {
+    switch (metric.name) {
+        case 'FCP':
+            break
+        case 'LCP':
+            break
+        case 'CLS':
+            break
+        case 'FID':
+            break
+        case 'TTFB':
+            break
+        case 'Next.js-hydration':
+            break
+        case 'Next.js-route-change-to-render':
+            break
+        case 'Next.js-render':
+            break
+        default:
+            break
+    }
+}
